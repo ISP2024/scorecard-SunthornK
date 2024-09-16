dc6a43b708e8cd8a5a635e0db3ae21329af8dc98
SunthornK <sunthorn.ko@ku.th>
2024-09-16 14:11:44 +0700

    finish all task

--- scorecard.py
+++ scorecard.py
@@ -17,25 +17,36 @@ Observe how the type hint helps it perform static checking.
    Include the type of keys and values.
 
 """
+from collections.abc import Iterable, Sized
 
 
-class Scorecard:
+class Scorecard(Iterable,Sized):
     """Accumulate scores and compute their average."""
 
     def __init__(self):
-        """Iniiialize a new Scorecard."""
-        self.scores = []
+        """Initialize a new Scorecard."""
+        self.scores: list[float] = []
 
-    def add_score(self, score):
+    def __len__(self) -> int:
+        """the size is just the number of scores"""
+        return len(self.scores)
+
+    def __iter__(self):
+        """return an iterator for scores"""
+        return iter(self.scores)
+
+    def add_score(self, score: float):
         """Add a score to the Scorecard."""
         self.scores.append(score)
 
-    def average(self):
+    def average(self) -> float:
         """Return the average of all scores, 0 if no scores."""
-        return sum(self.scores)/max(1,len(self.scores))
+        # add the values of the score objects
+        total = sum(float(x) for x in self.scores)
+        # don't divide by zero if no scores
+        return total / max(1, len(self.scores))
 
-
-def print_scores(score_card):
+def print_scores(score_card:Scorecard):
     """Print statistics for the scorecard and the actual scores."""
 
     # What changes to Scorecard are needed in order to make this code work?
@@ -46,12 +57,12 @@ def print_scores(score_card):
         print(score)
 
 
-def ordinal(num):
+def ordinal(num:int) -> str:
     """Return the ordinal value of an integer; works for numbers up to 20.
 
     For examples: ordinal(1) is '1st', ordinal(2) is '2nd'.
     """
-    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    suffixes:dict[int:str] = {1: "st", 2: "nd", 3: "rd"}
     return str(num) + suffixes.get(num, "th")
 
 
@@ -61,9 +72,9 @@ if __name__ == "__main__":
 
     print("Input 3 scores.")
     for count in range(1,4):
-        score = input(f"input {ordinal(count)} score: ")
+        score = float(input(f"input {ordinal(count)} score: "))
         scorecard.add_score(score)
 
-    print("The average is " + scorecard.average())
+    print("The average is ",scorecard.average())
 
     print_scores(scorecard)
